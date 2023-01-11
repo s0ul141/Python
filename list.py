fd8f6448395cae39ffa27eb373b24d2fedb7ea5d
Soumya_Basu <basusoumya366@gmail.com>
2023-01-11 23:54:35 +0530

    list_1.0

---
+++ list.py
@@ -0,0 +1,14 @@
+'''Accept a sequence of comma-separated integers as input and print the maximum value in the sequence as output.'''
+
+print ("a list of number separated by commas")
+nums = input().split(',')
+max_num = int(nums[0])
+for i in nums:
+    i = int(i)
+    if i > max_num:
+        max_num = i
+        
+print("the max number is",+max_num)
+#sorting the list that is given by the user
+nums.sort()
+print (nums)
