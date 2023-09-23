2495d0d2f0e7521756ca1d535b47ce8a0e75823b
Soumya Basu <s0uL141>
2023-09-23 12:22:16 +0530

    idk

---
+++ me.java
@@ -0,0 +1,23 @@
+class printstream
+{
+			void printoo(String a)
+			{
+				System.out.print(a);
+			}	
+}
+
+final class pystem
+{
+	printstream outto = new printstream();
+
+}
+
+class me
+{
+	public static void main (String[]args)
+	{
+		pystem outto = new pystem();
+		outto.printoo("I am lol");
+		
+	}
+}
